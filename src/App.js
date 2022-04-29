033238609a96a44be44d93ee7d78d8a7cdade1a0
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-29 23:12:03 +0600

    appointment ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {BiCalendar} from 'react-icons/bi'
+import Appoinment from './components/Appoinment/Appoinment';
 import Search from './components/Search/Search';
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
         <BiCalendar className="inline-block text-red-500 align-top" />Your Appointments</h1>
+        <Appoinment></Appoinment>
         <Search></Search>
     </div>
   );
