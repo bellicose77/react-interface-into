9b5036abc8ff21ff0efea143b31d4593e9ca2a81
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-29 22:22:48 +0600

    dropdown com

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
-        <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
+        <BiCalendar className="inline-block text-red-500 align-top" />Your Appointments</h1>
         <Search></Search>
     </div>
   );
