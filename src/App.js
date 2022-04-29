01d55039e799a2cb6fe6570ee4753cf2c3f2a617
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-29 22:13:53 +0600

    serach

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import {BiCalendar} from 'react-icons/bi'
+import Search from './components/Search/Search';
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
         <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
+        <Search></Search>
     </div>
   );
 }
