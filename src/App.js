9b52a03fc1e61d6c5094af9fd1d0318e2349a2c9
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-29 21:43:33 +0600

    react-icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-
+import {FcCalendar} from 'react-icons/fc'
 function App() {
   return (
     <div>
+      <FcCalendar/>
        <h1 className="text-xl font-bold underline">
         Hello world!
     </h1>
