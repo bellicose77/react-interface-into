69c3eae98f7c9458553954be578995fdb41d75a5
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-02 13:38:14 +0600

    sort button

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -39,7 +39,7 @@ const Search = () => {
             className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
           <div className="absolute inset-y-0 right-0 flex items-center">
             <div>
-              <button type="button"
+              <button onClick={()=>setToggleSort(!toggleSort)} type="button"
                 className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
                 Sort By <BiCaretDown className="ml-2" />
               </button>
