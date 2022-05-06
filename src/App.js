87d69742d9bc2b34e6593c163264258625a278dc
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-06 23:13:42 +0600

    new hooki

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import {BiCalendar} from 'react-icons/bi'
 import Appoinment from './components/Appoinment/Appoinment';
 import AppointmentInfo from './components/AppointmentInfo/AppointmentInfo';
 import Search from './components/Search/Search';
 import appoinmentList from './data.json'
 function App() {
+  const [appoinmentList,setAppoinmentList]=useState([]);
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
