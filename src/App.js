546cb39f379311e12bf9bde7f5e0d89a78bbc43d
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-07 00:33:08 +0600

    fetch done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import {BiCalendar} from 'react-icons/bi'
 import Appoinment from './components/Appoinment/Appoinment';
 import AppointmentInfo from './components/AppointmentInfo/AppointmentInfo';
 import Search from './components/Search/Search';
-import appoinmentList from './data.json'
 function App() {
   const [appoinmentList,setAppoinmentList]=useState([]);
   const fetchData = useCallback(()=>{
-    fetch('../public/data.json')
-  },[])
+    fetch('./data.json')
+    .then(res=>res.json())
+    .then(data=>setAppoinmentList(data))
+  },[]);
+  useEffect(()=>{
+    fetchData();
+  },[fetchData])
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
