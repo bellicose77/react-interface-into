8e245b0489c0ca7143c000a510394bc6b1d9ee1d
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-07 17:59:26 +0600

    search button start

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -38,7 +38,7 @@ const Search = () => {
             <BiSearch />
             <label htmlFor="query" className="sr-only" />
           </div>
-          <input type="text" name="query" id="query" value=""
+          <input onChange={(e)=>onChangeQuery(e)} type="text" name="query" id="query" value=""
             className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
           <div className="absolute inset-y-0 right-0 flex items-center">
             <div>
