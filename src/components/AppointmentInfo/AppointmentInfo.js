518a3ce41a57bd5b6500dd3b9bcfef265a2d1308
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-07 16:47:20 +0600

    delete button

--- src/components/AppointmentInfo/AppointmentInfo.js
+++ src/components/AppointmentInfo/AppointmentInfo.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {BiTrash} from 'react-icons/bi'
 
 const AppointmentInfo = ({appointment}) => {
-    const {petName,ownerName,aptDate}=appointment
+    const {id,petName,ownerName,aptDate}=appointment
     return (
         <li className="px-3 py-3 flex items-start">
-      <button type="button"
+      <button onClick={deleteHandler} type="button"
         className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
         <BiTrash /></button>
       <div className="flex-grow">
